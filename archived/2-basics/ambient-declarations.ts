502f86b752465ecc7431c9ea2c84fd97d62dff88
Bowen Cheng <blueicycky@gmail.com>
2018-04-18 08:52:16 -0400

    Remove old files

--- archived/2-basics/ambient-declarations.ts
+++
@@ -1,8 +0,0 @@
-//"Document" type is referenced from lib.d.ts, which is referenced by default
-declare var document: Document;
-document.title = "Hello";
-
-// Imports typing from jquery.d.ts file
-///<reference path="jquery.d.ts"/>
-declare  let $;
-$('div').text("Hello");
