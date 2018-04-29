78c88131d96c580515b6a164276c1654abe97cfc
Bowen Cheng <blueicycky@gmail.com>
2018-04-29 08:40:51 -0400

    Elaborate types
    This change demonstrates:
    * type assertion
    * union types
    * strictNullChecks compiler feature
    * let/const vs var

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,21 @@
 function startGame() {
-  var messageElement = document.getElementById('messages');
-  messageElement.innerText = 'Welcome! Starting a new game...'
+  let playerName: string | undefined;
+  // playerName = null; // error
+  // playerName = undefined; // works
+  playerName = 'Billy'; // works
+
+  // logPlayer(null); // error
+  // logPlayer(undefined); // works
+  logPlayer(playerName);
+
+  const messageElement: HTMLElement | null = document.getElementById('messages');
+  // messageElement.innerText = 'Welcome! Starting a new game...'; // TS reports object could be null
+  // non-null assertion operator tells the compiler "this won't be null/undefined so don't complain"
+  messageElement!.innerText = 'Welcome! Starting a new game...';
 }
 
-document.getElementById('startGame').addEventListener('click', startGame);
+function logPlayer(name: string | undefined) {
+  console.log(`New game starting for player: ${name}`);
+}
+
+document.getElementById('startGame')!.addEventListener('click', startGame);
