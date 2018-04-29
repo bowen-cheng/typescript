0ae73109e75befb90a6120884657f1649420b5cc
Bowen Cheng <blueicycky@gmail.com>
2018-04-29 17:46:55 -0400

    Demonstrate more things about variables and parameters
    Including:
    * defalut value for function parameters
    * optional parameters for functions
    * non-null assertion operator
    * noImplicitAny: forbits function f(p) {...} since p is implicitly "any"

--- app/app.ts
+++ app/app.ts
@@ -1,21 +1,38 @@
-function startGame() {
+function startGame(): void {
   let playerName: string | undefined;
   // playerName = null; // error
   // playerName = undefined; // works
-  playerName = 'Billy'; // works
+  // playerName = 'Billy'; // works
+  playerName = getInputValue('playername');
 
   // logPlayer(null); // error
   // logPlayer(undefined); // works
   logPlayer(playerName);
 
-  const messageElement: HTMLElement | null = document.getElementById('messages');
+  // const messageElement: HTMLElement | null = document.getElementById('messages');
   // messageElement.innerText = 'Welcome! Starting a new game...'; // TS reports object could be null
-  // non-null assertion operator tells the compiler "this won't be null/undefined so don't complain"
-  messageElement!.innerText = 'Welcome! Starting a new game...';
+  // "!" is the non-null assertion operator tells the compiler "this won't be null/undefined so don't complain"
+  // messageElement!.innerText = 'Welcome! Starting a new game...';
+  postScore(100, playerName)
 }
 
-function logPlayer(name: string | undefined) {
+function logPlayer(name: string = 'Unknown player'): void {
   console.log(`New game starting for player: ${name}`);
+}
+
+// function postScore(score: number, playerName?: string): void { // "?" marks a parameter as optional
+function postScore(score: number, playerName: string = 'Unknown player'): void {
+  let scoreElement: HTMLElement = <HTMLElement> document.getElementById('postedScores');
+  scoreElement!.innerText = `${score} - ${playerName}`;
+}
+
+function getInputValue(elementId: string): string | undefined {
+  let inputElement: HTMLInputElement = <HTMLInputElement> document.getElementById(elementId);
+  if (inputElement.value === '') {
+    return undefined;
+  }  else {
+    return inputElement.value;
+  }
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
